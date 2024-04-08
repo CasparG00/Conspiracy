21a88defd8d1f110a4921305874daddddc01c2ac
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-04-08 17:48:06 +0200

    wip

--- game_manager.gd
+++ game_manager.gd
@@ -1,1 +1,17 @@
 extends Node
+
+
+var tutorial_events := [
+	"You are a conspiracy theory. \n
+	This is a sheep.",
+
+	func(): print("spawn sheep"), # Make them go to the player.
+
+	"Sheep look for truth. \n
+	Some seek out your truth, others will find the real truth.",
+
+	"If a sheep will not find your truth, \n
+	you can give them a nudge by putting them in denial about the truth they seek.",
+	
+	func(): print("spawn sheep"), # Make them go to  the other one
+]
